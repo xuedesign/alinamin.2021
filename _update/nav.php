58073644e30e3d6e95629156a73d8dd190d0ea95
ElmoWoo <hello@xuedesign.tw>
2022-06-24 15:19:30 +0800

    修正頁面 #2022-06-24
    -主選單連結修正
    -文章頁面號碼移除

--- _update/nav.php
+++ _update/nav.php
@@ -7,10 +7,10 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <div class="content flex-b-c">
         <a href="<?php echo $rootURL;?>." class="logo" g-event="Nav_takeda台灣武田Logo"></a>
         <nav>
-            <a href="blogs.html">容易維生素B1缺乏的族群</a>
+            <a href="<?php echo $rootURL;?>blogs.html">容易維生素B1缺乏的族群</a>
             <a <?php echo ($page=="cause")?'class="current"':'';?> href="<?php echo $rootURL;?>cause" g-event="Nav_疲酸麻的原因">你的疲。酸。麻原因</a>
             <a <?php echo ($page=="fursultiamine")?'class="current"':'';?> href="<?php echo $rootURL;?>fursultiamine" g-event="Nav_醫療級B1 TTFD">合利他命醫療級B群</a>
-            <a href="newpackage.html">合利他命新包裝上市</a>
+            <a href="<?php echo $rootURL;?>newpackage.html">合利他命新包裝上市</a>
             <a <?php echo ($page=="lineup")?'class="current"':'';?> href="<?php echo $rootURL;?>lineup" g-event="Nav_全產品列表">全產品列表</a>
             <div class="nav-more" g-event="Nav_了解更多">
                 了解更多
@@ -20,7 +20,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                     <a href="<?php echo $rootURL;?>explus" g-event="Nav_了解更多_EXPLUS強效錠">EX PLUS 強效錠</a>
                     <a href="<?php echo $rootURL;?>a25" g-event="Nav_了解更多_愛A25">愛A25</a>
                     <a href="<?php echo $rootURL;?>buy/#pcs" g-event="Nav_了解更多_購買通路">購買通路</a>
-                 <!--    <a href="<?php //echo $rootURL;?>QA/#all" g-event="Nav_了解更多_常見問題Q&A">常見問題Q&A</a> -->
+                    <a href="<?php echo $rootURL;?>QA" g-event="Nav_了解更多_常見問題Q&A">常見問題Q&A</a>
                     <a href="<?php echo $rootURL;?>history" g-event="Nav_了解更多_合利他命藥品">合利他命藥品</a>
                 </div>
             </div>
@@ -38,10 +38,10 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 </div>
 <div class="menu-wrap">
     <div class="menuBox">
-        <a href="blogs.html">維生素B1缺乏的族群</a>
+        <a href="<?php echo $rootURL;?>blogs.html">維生素B1缺乏的族群</a>
         <a href="<?php echo $rootURL;?>cause"  g-event="Nav_疲酸麻的原因">你的疲。酸。麻原因</a>
         <a href="<?php echo $rootURL;?>fursultiamine"  g-event="Nav_Nav_醫療級B1 TTFD">合利他命醫療級B群</a>
-        <a href="newpackage.html">合利他命新包裝上市</a>
+        <a href="<?php echo $rootURL;?>newpackage.html">合利他命新包裝上市</a>
         <a href="<?php echo $rootURL;?>lineup"  g-event="Nav_全產品列表">全產品列表</a>
         <a href="<?php echo $rootURL;?>." class="menu-home"  g-event="Nav_HOME">回首頁</a>
     </div>
@@ -50,7 +50,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <a href="<?php echo $rootURL;?>explus" g-event="Nav_了解更多_EXPLUS強效錠">EX PLUS 強效錠</a>
         <a href="<?php echo $rootURL;?>a25" g-event="Nav_了解更多_愛A25">愛A25</a>
         <a href="<?php echo $rootURL;?>buy/#pcs" g-event="Nav_了解更多_購買通路">購買通路</a>
-<!--         <a href="<?php //echo $rootURL;?>QA/#all" g-event="Nav_了解更多_常見問題Q&A">常見問題Q&A</a> -->
+        <a href="<?php echo $rootURL;?>QA" g-event="Nav_了解更多_常見問題Q&A">常見問題Q&A</a>
         <a href="<?php echo $rootURL;?>history" g-event="Nav_了解更多_合利他命藥品">合利他命藥品</a>
     </div>
 </div>
